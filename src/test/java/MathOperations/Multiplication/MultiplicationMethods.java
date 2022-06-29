24ca2e4b4ec7d1694ac6190324321867d2a21fa0
Ira <ira@MacBook-Air-Ira.local>
2022-06-29 16:24:42 +0300

    mathOperations2

---
+++ src/test/java/MathOperations/Multiplication/MultiplicationMethods.java
@@ -0,0 +1,11 @@
+package MathOperations.Multiplication;
+
+public class MultiplicationMethods {
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+}
